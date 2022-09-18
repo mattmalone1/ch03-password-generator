da5f930cb992adee6be809f62903c655fd68e5f0
Matthew Malone <matthewmalone3@gmail.com>
2022-09-18 02:27:08 -0400

    Dynamically ceate a string to draw random characters from

--- script.js
+++ script.js
@@ -16,31 +16,54 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   let passwordLength = 0;
+  
+  // Create strings of the characters that we can draw from.
+  const lower ='abcdefghijklmnopqrstuvwxyz';
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const number = '0123456789';
+  const special = '!\"#$%&\'()*,-./:;<=>?@[\\]^_`{|}~';
   // prompt for the password length keep prompting until the password length is valid.
   do{
     passwordLength = window.prompt("Please enter a a password length between 8 and 128.");
   }  while((passwordLength < 7) && (passwordLength > 129));
 
   // console.log(passwordLength);
+  // create an empty string.
+  let charsSelected = '';
 
   // get user password selections
-  let incLower = window.confirm("Include lowercase characters?");
-  let incUpper = window.confirm("Include uppercase characters?");
-  let numeric = window.confirm("Include numeric characters?");
-  let specChar = window.confirm("Include Special Characters?");
+  let incLower = window.confirm('Include lowercase characters?');
+  let incUpper = window.confirm('Include uppercase characters?');
+  let numeric = window.confirm('Include numeric characters?');
+  let specChar = window.confirm('Include Special Characters?');
 
+  // create the conditions and add them to the randomized string.
+  if(incLower === true) {
+    charsSelected = charsSelected.concat(lower);
+  }
+  if(incUpper === true) {
+    charsSelected = charsSelected.concat(upper);
+  }
+  if(numeric === true) {
+    charsSelected = charsSelected.concat(number);
+  }
+  if(specChar === true) {
+    charsSelected = charsSelected.concat(special);
+  }
+
+  // console.log(charsSelected);
   //Create an array to hold the password letters.
-  let passwordSelect = Array(passwordLength);
+  let passwordSelect = '';
 
   let i = 0; 
   while(i < passwordLength){
 
-    // This construction is Unicode and excludes the space when getting a random character.
-    passwordSelect[i] = String.fromCharCode(0x0021 + Math.random() * (0x007E-0x0021+1));
+    // select a random character from the string and build passwordSelect.
+    passwordSelect += charsSelected.charAt(Math.floor(Math.random() * charsSelected.length));
     i++;
   }
 
-  return passwordSelect.join("");
+  return passwordSelect;
 
 
   }
